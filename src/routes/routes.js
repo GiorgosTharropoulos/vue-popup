dfc08839e07ea450b66989139170e1e9c413787e
Giorgos Tharropoulos <tharropoulos@gmail.com>
2022-01-24 11:09:43 +0200

    Dev dependencies

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,3 @@
 import SidebarNavLink from '../models/navigation/SidebarNavLink';
 
-export default [
-  new SidebarNavLink('Home', '/', 'mdi-home'),
-  new SidebarNavLink('Pop-Ups', '/pop-ups', 'mdi-application-edit'),
-  new SidebarNavLink('Tags', '/tags', 'mdi-tag'),
-];
+export default [new SidebarNavLink('Pop-Ups', '/', 'mdi-application-edit')];
